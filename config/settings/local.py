510ec7f4b990c4955d7054b52e3f7fd84b41f2c1
Sandra Milosevic <smilosevic892@gmail.com>
2026-07-23 16:24:46 +0200

    chore: split settings into base, local and production for easier managment

---
+++ config/settings/local.py
@@ -0,0 +1,29 @@
+from os import getenv, path
+from dotenv import load_doenv
+from .base import *  # noqa
+from .base import BASE_DIR
+
+local_env = path.join(BASE_DIR, ".envs", "env.local")
+
+if path.isfile(local_env):
+    load_doenv(local_env)
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = getenv("SECRET_KEY")
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = getenv("DEBUG")
+
+SITE_NAME = getenv("SITE_NAME")
+
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+
+ADMIN_URL = getenv("ADMIN_URL")
+
+EMAIL_BACKEND = "djcelery_email.backends.CeleryEmailBackend"
+EMAIL_HOST = getenv("EMAIL_HOST")
+EMAIL_PORT = getenv("EMAIL_PORT")
+DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
+DOMAIN = getenv("DOMAIN")
+
+MAX_UPLOAD_SIZE = 1 * 1024 * 1024
