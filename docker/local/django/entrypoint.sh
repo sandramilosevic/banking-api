2460f188b289e95ef22933862a8268e5a6e6d04c
Sandra Milosevic <smilosevic892@gmail.com>
2026-07-26 22:03:35 +0200

    feat: add Docker configuration with entrypoint and start scripts for Django application

---
+++ docker/local/django/entrypoint.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+set -o errexit
+
+set -o pipefail
+
+set -o nounset
+
+python << END
+import sys
+import time
+import psycopg2
+suggest_unrecoverable_after = 30
+start = time.time()
+while True:
+    try:
+        psycopg2.connect(
+        dbname="${POSTGRES_DB}",
+        user="${POSTGRES_USER}",
+        password="${POSTGRES_PASSWORD}",
+        host="${POSTGRES_HOST}", 
+        port="${POSTGRES_PORT}",)
+        break
+    except pycopg2.OperationError as error:
+        sys.stderr.write("Waiting for PostgreSQL to become available..\n")
+        if time.time() - start > suggest_unrecoveravle_after:
+            sys.stderr.write("This is taking longer than excepted. The following exception may be indicative of an unrecoverable error: '{}'\n".format(error))
+            time.sleep(3)
+
+END
+
+echo >&2 'PostgreSQL is available'
+
+exec "$@"
+
