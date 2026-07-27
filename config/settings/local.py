95260a22cf2e8fa8feb23207a34833a404725f3e
Sandra Milosevic <smilosevic892@gmail.com>
2026-07-27 19:20:33 +0200

    feat: update environment configuration and add CSRF settings; create email templates for account lock and OTP notifications

--- config/settings/local.py
+++ config/settings/local.py
@@ -2,6 +2,7 @@ from os import getenv, path
 from dotenv import load_dotenv
 from .base import *  # noqa
 from .base import BASE_DIR
+from datetime import timedelta
 
 local_env = path.join(BASE_DIR, ".env")
 
@@ -27,3 +28,18 @@ DEFAULT_FROM_EMAIL = getenv("DEFAULT_FROM_EMAIL")
 DOMAIN = getenv("DOMAIN")
 
 MAX_UPLOAD_SIZE = 1 * 1024 * 1024
+
+csrf_origins = getenv("CSRF_TRUSTED_ORIGINS")
+if not csrf_origins:
+    raise ValueError("CSRF_TRUSTED_ORIGINS is not set in .env")
+
+CSRF_TRUSTED_ORIGINS = [o.strip() for o in csrf_origins.split(",") if o.strip()]
+
+# how long user is lockout after multiple failed logins (production >= 10 mins)
+LOCKOUT_DURATION = timedelta(minutes=1)
+
+# failed logins before lockout
+LOGIN_ATTEMPTS = 3
+
+# Expiration time
+OTP_EXPIRATION = timedelta(minutes=1)
