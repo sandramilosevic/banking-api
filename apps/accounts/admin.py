8c97ad77e0ecae7e51bc572414e230eddf5075ea
Sandra Milosevic <smilosevic892@gmail.com>
2026-07-28 22:39:39 +0200

    feat: implement CustomerUserAdmin for User model with custom list display and fieldsets

--- apps/accounts/admin.py
+++ apps/accounts/admin.py
@@ -1,3 +1,48 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.utils.translation import gettext_lazy as _
+from .models import User
+from .forms import UserChangeForm, UserCreationForm
 
-# Register your models here.
+
+@admin.register(User)
+class CustomerUserAdmin(UserAdmin):
+    form = UserChangeForm
+    add_form = UserCreationForm
+    model = User
+    list_display = [
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+        "role",
+    ]
+    list_filter = ["email", "is_staff", "is_active", "role"]
+    fieldsets = (
+        (
+            _("Login credentials"),
+            {
+                "fields": (
+                    "username",
+                    "emails",
+                    "password",
+                )
+            },
+        ),
+        (
+            _("Personal info"),
+            {"fields": ("first_name", "last_name", "id_no", "role")},
+        ),
+        (
+            _("Account status"),
+            {
+                "fields": (
+                    "account_status",
+                    "failed_login_attempts",
+                    "last_failed_login",
+                )
+            },
+        ),
+    )
